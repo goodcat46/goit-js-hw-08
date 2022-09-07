7a13ea3c9c3bbbe0eb1249f3e4b949c00c3255b3
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-09-07 18:04:18 +0300

    2й пункт зафіксив

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,39 +7,33 @@ const player = new Player(iframe);
 
 // todo Отримую час із localstorage у змінну для зручності
 let getActualTimePoint = localStorage.getItem('videoplayer-current-time');
-// todo Записую час у localstorage
 setVideoCurrentTime();
+//  todo Виводжу у консолль час із котрого почався перегляд
+player.on('play', function () {
+  // todo Записую час у localstorage
+  player.on(
+    'timeupdate',
+    throttle(timeData => {
+      localStorage.setItem('videoplayer-current-time', timeData.seconds);
+    }, 1000)
+  );
+  console.log(
+    `played the video! At ${localStorage.getItem('videoplayer-current-time')}`
+  );
+  // todo Виводжу у консоль час на якому ПЕРЕРВАЛИ перегляд
+  player.on('pause', function () {
+    console.log(
+      `paused the video! At ${localStorage.getItem('videoplayer-current-time')}`
+    );
+  });
+});
+
 function setVideoCurrentTime() {
   if (getActualTimePoint !== null) {
     // todo Встановлюю час на якому зупинилось відео коли користувач вернеться на сторінку
     player.setCurrentTime(getActualTimePoint);
     console.log('videoplayer-current-time', getActualTimePoint);
-
-    //  todo Виводжу у консолль час із котрого почався перегляд
-    player.on('play', function () {
-      console.log(
-        `played the video! At ${localStorage.getItem(
-          'videoplayer-current-time'
-        )}`
-      );
-    });
-    // todo Виводжу у консоль час на якому ПЕРЕРВАЛИ перегляд
-    player.on('pause', function () {
-      console.log(
-        `paused the video! At ${localStorage.getItem(
-          'videoplayer-current-time'
-        )}`
-      );
-    });
     return;
   }
   console.log(`ВІдео ще не переглядали`);
 }
-
-// todo Перезапис часу
-player.on(
-  'timeupdate',
-  throttle(timeData => {
-    localStorage.setItem('videoplayer-current-time', timeData.seconds);
-  }, 1000)
-);
