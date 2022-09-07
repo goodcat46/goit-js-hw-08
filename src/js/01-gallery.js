60ad53bf19abe0fef67b515452c610abedecad04
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-09-07 11:49:18 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+// console.log(galleryItems);
+
+let galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+
+function createGalery() {
+    const createGalleryItem = element => {
+      const { preview, original, description } = element;
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    };
+  
+    const galleryItemsArray = (galleryItems.map(el => {
+      return createGalleryItem(el);
+    })).join('');
+  
+    galleryEl.insertAdjacentHTML('afterbegin', galleryItemsArray);
+  }
+  createGalery();
+
+
+  let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+console.log(iframe);
 
+const player = new Player(iframe);
+console.log(player);
+
+// todo Перезапис часу
+player.on('timeupdate', function (timeData) {
+    //todo Записую час у localstorage 
+    localStorage.setItem('Актуальний часовий поінт', timeData.seconds);
+});
+
+// todo Отримую час із localstorage у змінну для зручності
+let getPausedTime = localStorage.getItem('Актуальний часовий поінт');
+console.log('Актуальний часовий поінт', getPausedTime);
+// todo Встановлюю час на якому зупинилось відео коли користувач вернеться на сторінку
+player.setCurrentTime(getPausedTime)
+
+
+player.on('play', function () {
+    console.log(`played the video! At ${localStorage.getItem('Актуальний часовий поінт')}`);
+});
+player.on('pause', function () {
+    console.log(`paused the video! At ${localStorage.getItem('Актуальний часовий поінт')}`);
+});
