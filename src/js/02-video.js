e6cf9699f6a928e397249ee0988d6d61088fff31
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-09-07 17:43:38 +0300

    2й пункт завдання готовий

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,45 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
-console.log(iframe);
-
+// console.log(iframe);
+// todo Ініціалізація
 const player = new Player(iframe);
-console.log(player);
-
-// todo Перезапис часу
-player.on('timeupdate', function (timeData) {
-    //todo Записую час у localstorage 
-    localStorage.setItem('Актуальний часовий поінт', timeData.seconds);
-});
 
 // todo Отримую час із localstorage у змінну для зручності
-let getPausedTime = localStorage.getItem('Актуальний часовий поінт');
-console.log('Актуальний часовий поінт', getPausedTime);
-// todo Встановлюю час на якому зупинилось відео коли користувач вернеться на сторінку
-player.setCurrentTime(getPausedTime)
+let getActualTimePoint = localStorage.getItem('videoplayer-current-time');
+// todo Записую час у localstorage
+setVideoCurrentTime();
+function setVideoCurrentTime() {
+  if (getActualTimePoint !== null) {
+    // todo Встановлюю час на якому зупинилось відео коли користувач вернеться на сторінку
+    player.setCurrentTime(getActualTimePoint);
+    console.log('videoplayer-current-time', getActualTimePoint);
 
+    //  todo Виводжу у консолль час із котрого почався перегляд
+    player.on('play', function () {
+      console.log(
+        `played the video! At ${localStorage.getItem(
+          'videoplayer-current-time'
+        )}`
+      );
+    });
+    // todo Виводжу у консоль час на якому ПЕРЕРВАЛИ перегляд
+    player.on('pause', function () {
+      console.log(
+        `paused the video! At ${localStorage.getItem(
+          'videoplayer-current-time'
+        )}`
+      );
+    });
+    return;
+  }
+  console.log(`ВІдео ще не переглядали`);
+}
 
-player.on('play', function () {
-    console.log(`played the video! At ${localStorage.getItem('Актуальний часовий поінт')}`);
-});
-player.on('pause', function () {
-    console.log(`paused the video! At ${localStorage.getItem('Актуальний часовий поінт')}`);
-});
+// todo Перезапис часу
+player.on(
+  'timeupdate',
+  throttle(timeData => {
+    localStorage.setItem('videoplayer-current-time', timeData.seconds);
+  }, 1000)
+);
